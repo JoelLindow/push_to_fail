a805f2929b09b00724a9d88237f63ae2bc873873
JoelLindow <joellindow@gmail.com>
2018-09-18 19:24:18 -0600

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: '/failures'
+  root to: 'failures#index'
   resources :failures
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
