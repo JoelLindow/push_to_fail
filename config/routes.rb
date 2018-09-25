6e47e398aa8e95e5d76caaedbf277f3a56aa0bd7
JoelLindow <joellindow@gmail.com>
2018-09-24 20:34:26 -0600

    Authentication working. Need to modify views conditionally for current_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   root to: 'failures#dashboard'
-  resources :failures
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :failures, only: [:index, :new]
+
+  # sign up page with form:
+  get 'users/new' => 'users#new', as: :new_user
+  # create (post) action for when sign up form is submitted:
+  post 'users' => 'users#create'
+
+  # log in page with form:
+	get '/login'     => 'sessions#new'
+	# create (post) action for when log in form is submitted:
+	post '/login'    => 'sessions#create'
+	# delete action to log out:
+	delete '/logout' => 'sessions#destroy'
 end
