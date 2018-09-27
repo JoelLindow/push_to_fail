ea914dcc03f7f9167a76395fad8b18461d2022d9
JoelLindow <joellindow@gmail.com>
2018-09-26 19:00:46 -0600

    migration add - add user ID to Failure records table.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Failure.create(kind: "Pushups", count: 1)
-Failure.create(kind: "Pushups", count: 2)
-Failure.create(kind: "Pushups", count: 3)
+User.create(name: "Johnny User", email: "test@test.com", password: "test")
 
-Failure.create(kind: "Situps", count: 1)
-Failure.create(kind: "Situps", count: 2)
-Failure.create(kind: "Situps", count: 3)
+Failure.create(kind: "Pushups", count: 1, user_id: 1)
+Failure.create(kind: "Pushups", count: 2, user_id: 1)
+Failure.create(kind: "Pushups", count: 3, user_id: 1)
 
-Failure.create(kind: "Jumping Jacks", count: 1)
-Failure.create(kind: "Jumping Jacks", count: 2)
-Failure.create(kind: "Jumping Jacks", count: 3)
+Failure.create(kind: "Situps", count: 1, user_id: 1)
+Failure.create(kind: "Situps", count: 2, user_id: 1)
+Failure.create(kind: "Situps", count: 3, user_id: 1)
 
-Failure.create(kind: "Squats", count: 1)
-Failure.create(kind: "Squats", count: 2)
-Failure.create(kind: "Squats", count: 3)
+Failure.create(kind: "Jumping Jacks", count: 1, user_id: 1)
+Failure.create(kind: "Jumping Jacks", count: 2, user_id: 1)
+Failure.create(kind: "Jumping Jacks", count: 3, user_id: 1)
+
+Failure.create(kind: "Squats", count: 1, user_id: 1)
+Failure.create(kind: "Squats", count: 2, user_id: 1)
+Failure.create(kind: "Squats", count: 3, user_id: 1)
